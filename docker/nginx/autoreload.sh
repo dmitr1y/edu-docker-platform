28f88042e714a58b4e3d61696cea9851cdfeab11
Dmitriy Konstantinov <dmitriy@konstantinov.com.ru>
2018-10-06 02:01:43 +0300

    added autoreload script by changing nginx confs

---
+++ docker/nginx/autoreload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "--- starting Nginx ---">&2
+/usr/sbin/nginx -g "daemon off;"
+
+echo "--- starting Nginx conf watcher ---">&2
+DIR="/etc/nginx/conf.d"
+inotifywait -m -r -e moved_to -e create -e delete "$DIR" --format "%f" | while read f
+do
+    echo " --- reloading Nginx configuration  ---">&2
+     /usr/sbin/nginx -t
+    if [ $? -eq 0 ]
+    then
+        /usr/sbin/nginx -s reload
+    else
+        echo "[ERROR] BAD CONFIGURATION ">&2
+    fi
+done
