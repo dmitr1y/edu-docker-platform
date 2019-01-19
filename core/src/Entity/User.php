0a05c7ac73fca7bfb810b564763b2368f52f4c93
Dmitriy Konstantinov <dmitriy@konstantinov.com.ru>
2019-01-20 00:38:21 +0300

    added user, rest, doctrine and other bundles

---
+++ core/src/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+// src/Entity/User.php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
