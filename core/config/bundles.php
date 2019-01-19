0a7573eafed18797adde4fd89bfcf5b7515395d9
Dmitriy Konstantinov <dmitriy@konstantinov.com.ru>
2019-01-20 00:46:51 +0300

    added fos oauth server

--- core/config/bundles.php
+++ core/config/bundles.php
@@ -15,4 +15,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
